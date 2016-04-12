d69c06c8f2e8939ca87eaa950f140d6f4d08cce1
Jacobth <jacob.thureson@gmail.com>
2016-04-12 10:16:21 +0200

    adding data from txt

--- app/src/main/java/com/example/jacobth/smart/StatActivity2.java
+++ app/src/main/java/com/example/jacobth/smart/StatActivity2.java
@@ -9,13 +9,16 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.*;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 /**
@@ -35,33 +38,7 @@ public class StatActivity2 extends Activity implements View.OnClickListener{
 
         Intent i = getIntent();
         String category = i.getExtras().getString("category");
-      //  setLayout(category);
-
-        BarChart chart = (BarChart) findViewById(R.id.chart);
-
-        ArrayList<BarEntry> entries = new ArrayList<>();
-        entries.add(new BarEntry(4f, 0));
-        entries.add(new BarEntry(8f, 1));
-        entries.add(new BarEntry(6f, 2));
-        entries.add(new BarEntry(12f, 3));
-        entries.add(new BarEntry(18f, 4));
-        entries.add(new BarEntry(9f, 5));
-
-        BarDataSet dataSet = new BarDataSet(entries, "# of Calls");
-
-        ArrayList<String> labels = new ArrayList<String>();
-        labels.add("January");
-        labels.add("February");
-        labels.add("March");
-        labels.add("April");
-        labels.add("May");
-        labels.add("June");
-
-        BarData data = new BarData(labels, dataSet);
-        chart.setData(data);
-
-        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
-        chart.animateY(2000);
+        setLayout(category);
     }
     @Override
     public void onClick(View view) {
@@ -69,23 +46,84 @@ public class StatActivity2 extends Activity implements View.OnClickListener{
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
     }
 
+    private void drawChart(String file) {
+        BarChart chart = (BarChart) findViewById(R.id.chart);
+        ArrayList<BarEntry> entries = getData(file);
+
+        BarDataSet dataSet = new BarDataSet(entries, "Number of crimes");
+        ArrayList<String> labels = getLabels(entries.size());
+        BarData data = new BarData(labels, dataSet);
+
+        chart.setData(data);
+        dataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+        chart.animateY(2000);
+    }
+
     private void setLayout(String item) {
-        ImageView imageView = (ImageView)findViewById(R.id.imageView);
         TextView textView = (TextView)findViewById(R.id.textHigh);
 
         switch (item) {
             case "Rån":
                 textView.setText("Anmälda rån per 100 000 invånare från och med år 1950");
-                imageView.setImageResource(R.drawable.robbery2);
+                drawChart("robberyper.txt");
                 break;
             case "Misshandel":
                 textView.setText("Anmäld misshandel per 100 000 invånare från och med år 1950");
-                imageView.setImageResource(R.drawable.violence);
+                drawChart("violenceper.txt");
                 break;
             case "Sexualbrott":
                 textView.setText("Anmälda sexualbrott per 100 000 invånare från och med år 1950");
-                imageView.setImageResource(R.drawable.sexual);
+                drawChart("violenceper.txt");
                 break;
         }
     }
+
+    private ArrayList<String> getLabels(int size) {
+        ArrayList<String> labels = new ArrayList<String>();
+        for(int i = 0; i < size; i++) {
+            if(i == 0) {
+                labels.add("1950");
+            }
+            else if(i == size/2) {
+                labels.add("1991");
+            }
+            else if(i == size-1) {
+                labels.add("2014");
+            }
+            else {
+                labels.add("");
+            }
+        }
+        return labels;
+    }
+
+    private ArrayList<BarEntry> getData(String file) {
+        int i = 0;
+        ArrayList<BarEntry> entries = new ArrayList<>();
+
+        try {
+            InputStream inputStream = getAssets().open(file);
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append(receiveString);
+                    entries.add(new BarEntry(Integer.parseInt(receiveString), i++));
+                }
+                inputStream.close();
+            }
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return entries;
+    }
+
 }
